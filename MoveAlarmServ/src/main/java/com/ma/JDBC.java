73ddf83ec90cff814f3dc19f673afc433ea1e325
White Mort <noppadet-ice@hotmail.com>
2015-10-18 12:41:04 +0700

    update server file ยังไม่ได้ทำ member

--- MoveAlarmServ/src/main/java/com/ma/JDBC.java
+++ MoveAlarmServ/src/main/java/com/ma/JDBC.java
@@ -1,7 +1,9 @@
 package com.ma;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -14,7 +16,7 @@ public class JDBC {
     public JDBC(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/mydatabase"+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/move_alarm"+
                 "?user=root&password=");
             if(connection!=null){
                 System.out.println("Database Connected");
@@ -24,7 +26,25 @@ public class JDBC {
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
 
+
+    public ResultSet sql(String cmd){
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try{
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(cmd);
+            if(stmt.execute(cmd)){
+                rs = stmt.getResultSet();
+            }
+        }catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return rs;
     }
 
     public void close(){
--- MoveAlarmServ/src/main/java/com/ma/MemberSystem.java
+++ MoveAlarmServ/src/main/java/com/ma/MemberSystem.java
@@ -1,6 +1,11 @@
 package com.ma;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
 
 /**
  * Created by Admin on 10/15/2015.
@@ -10,9 +15,22 @@ public class MemberSystem {
 
     JDBC jdbc = new JDBC();
 
-    public Member getMembemById(String idFacebook){
-        return new Member();
+    @RequestMapping("/connect")
+    public JDBC connect(){
+        return jdbc;
     }
 
+    @RequestMapping("/")
+    public String test(){
+        return "Hello Server";
+    }
+
+
+    @RequestMapping("/getMember")
+    public Member getMemberByID(@RequestParam(value = "userID",required = false)
+                                    String userID){
+        //handle member
+        return new Member();
+    }
 
 }
